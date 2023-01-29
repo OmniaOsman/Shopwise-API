d6967ff7f35db5886e031eac442517bb2aec08be
Omnia Osman <UGS.140547@ci.suez.edu.eg>
2023-01-29 18:51:33 +0200

    edit railway setting

--- shopwise/settings.py
+++ shopwise/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#9_7q+s0h(i)@&o#h5(efcj!@p!2(6kp-bc7mds*!y+%3sx&#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app']
 
 
 # Application definition
