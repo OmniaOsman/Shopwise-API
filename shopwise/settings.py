15483ae66435ca3ee11adf24c58043a1723328b8
Omnia Osman <UGS.140547@ci.suez.edu.eg>
2023-04-16 17:55:09 +0200

    update vercel settings

--- shopwise/settings.py
+++ shopwise/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
