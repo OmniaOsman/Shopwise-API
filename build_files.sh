f8d7a4c3ae59a16e6e37887c0c0d909fd270d599
Omnia Osman <UGS.140547@ci.suez.edu.eg>
2023-04-16 16:07:06 +0200

    edit build file

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
-
-# Install Python 3.10.7
-curl -O https://www.python.org/ftp/python/3.10.7/Python-3.10.7.tar.xz
-tar -xf Python-3.10.7.tar.xz
-cd Python-3.10.7
-./configure --enable-optimizations
-make altinstall
-
-# Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.10 get-pip.py
-
-# Install dependencies
-pip3.10 install -r requirements.txt
-
-# Collect static files
-python3.10 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python3.10.7 manage.py collectstatic --noinput
